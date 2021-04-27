5d156679ea4c3b2e03818e56ed2d24e36b653833
Zhao Yizhu <nestncc@outlook.com>
2021-04-27 20:14:28 +0800

    bind method and bind super class is ready

--- compiler/compiler.cpp
+++ compiler/compiler.cpp
@@ -2,12 +2,12 @@
  * @Author: Nestcc
  * @Date: 2021-04-27 18:43:46
  * @LastEditors: Nestcc
- * @LastEditTime: 2021-04-27 19:12:56
+ * @LastEditTime: 2021-04-27 20:14:05
  * @Description:  < file content > 
  */
 
 #include "headers.h"
-#include "../include/util.h"
+#include "../includes/util.h"
 #include "../vm/core.h"
 #include "compiler.h"
 #include "../object/ObjModule.h"
